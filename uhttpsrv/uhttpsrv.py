07ef1fc86cc35c631093cafc61ec2c434fba9652
Tim Pelling <tim.pelling@gmail.com>
2015-11-20 08:09:50 +0400

    Minor cleanup

--- uhttpsrv/uhttpsrv.py
+++ uhttpsrv/uhttpsrv.py
@@ -2,14 +2,14 @@ import socket
 
 class uHTTPsrv:
 
-	PROTECTED = [b'__init__', b'listen_once', b'listen', b'response_header', b'__qualname__', b'__module__', b'address', b'port', b'backlog', b'in_buffer_len', b'debug']
+	PROTECTED = ['__init__', 'listen_once', 'listen', 'response_header', '__qualname__', '__module__', 'address', 'port', 'backlog', 'in_buffer_len', 'debug']
 
-	def __init__(self, address='', port=80, backlog=1, in_buffer_len=1024, debug=False):
+	def __init__(self, address='', port=80, backlog=1, in_buffer_len=1024, DEBUG=False):
 		self.address = address
 		self.port = port
 		self.backlog = backlog
 		self.in_buffer_len = in_buffer_len
-		self.debug = debug
+		self.DEBUG = DEBUG
 		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self._socket.bind((self.address, self.port))
 		self._socket.listen(self.backlog)
@@ -18,20 +18,20 @@ class uHTTPsrv:
 		conn,addr = self._socket.accept()
 		request = conn.recv(self.in_buffer_len)
 		request = request.rsplit(b'\r\n')
-		if self.debug:
+		if self.DEBUG:
 			for line in request:
 				print(line)
+		#decode as hasattr doesn't like bytearray
 		method = request[0].rsplit(b' ')[0].decode('utf-8')
-		print(method)
 		if method.lower() not in self.PROTECTED:
 			if hasattr(self, method):
-				response = self.response_header(200) + \
-				    		eval('self.'+method+'(self,request)')
+				response = self.response_header(200) + eval('self.'+method+'(self,request)')
 			else:
 				response=self.response_header(501)
 		else:
 			response = self.response_header(501)
-		if self.debug:
+		print(response)
+		if self.DEBUG:
 			for line in response:
 				print(line)
 		conn.send(response)
@@ -41,5 +41,6 @@ class uHTTPsrv:
 		while True:
 			self.serve_one(self)
 
+	#may not work, response code may require trailing descriptive string
 	def response_header(self, code):
 		return b'HTTP/1.1 ' + str(code) + b'\nConnection: close\n\n'
