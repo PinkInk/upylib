c7974974e0bc2ba6bd8358dc3d741b8d926954f7
Tim Pelling <tim.pelling@gmail.com>
2016-04-06 09:02:18 +0400

    first stab at _SnmpPacket and GetRequest classes

--- usnmp/usnmp.py
+++ usnmp/usnmp.py
@@ -38,56 +38,100 @@ SNMP_ERR_BADVALUE = 0x03
 SNMP_ERR_READONLY = 0x04
 SNMP_ERR_GENERR = 0x05
 
-class GetRequest():
-    #should request classes derive from "SnmpPacket" or similar?
+class GetRequest(_SnmpPacket):
     def __init__(self, data=None, \
-                       ver=SNMP_VER1, community="public", request_id=1, mibs=[]
+                       ver=SNMP_VER1, community="wibble", id=1, oids=[]
                 ):
-        if type(data) is bytearray:
-            self._rawpacket = data
-            unpacked = unpack(data)
-            self.ver = unpacked[1][1]
-            self.community = unpacked[2][1]
-            self.request_id = unpacked[3][1][1]
-            self.error_status = unpacked[3][2][1]
-            self.index = unpacked[3][3][1]
-            if self.error_status == SNMP_ERR_NOERROR:
-                self.mibs = []
-                for seq in unpacked[3][4][1:]:
-                    self.mibs.append(seq[1][1])
-        else:
+        _SnmpPacket.__init__(self)
+        if data == None:
             self.ver = ver
             self.community = community
-            self.request_id = request_id
-            self.error_status = SNMP_ERR_NOERROR
-            self.index = 0
-            self.mibs = mibs
+            self.type = SNMP_GETREQUEST
+            self.id = id
+            for oid in oids:
+                self.addoid(oid)
+        else:
+            self._packet = data
+            pl = unpack(data)
+            if pl[1][2][0] != SNMP_GETREQUEST:
+                raise Exception('not a getrequest packet')
+            else:
+                self.ver = pl[1][0][1]
+                self.community = pl[1][1][1]
+                self.type = pl[1][0][1]
+                self.id = pl[1][2][1][0][1]
+                self.err_status = pl[1][2][1][1][1]
+                self._err_id = pl[1][2][1][2][1]
+                if self.err_status == SNMP_ERR_NOERROR:
+                    for seqtv in pl[1][2][1][3][1]:
+                        oid = seqtv[1][0][1]
+                        t,v = seqtv[1][1][0],seqtv[1][1][1]
+                        self.addoid(oid, (t,v))
+
+class _SnmpPacket():
+    def __init__(self):
+        self._ver = SNMP_VER1
+        self._community = ''
+        self._type = None
+        self._id = 1
+        self._err_status = 0x0
+        self._err_id = 0x0
+        self._oids = {}
+        self._packet = None
+    def getoid(self, oid):
+        try:
+            return self._oids[oid]
+        except:
+            return None
+    def addoid(self, oid, tv=None):
+        if tv == None:
+            self._oids[oid] = ASN1_NULL, None
+        else:
+            self._oids[oid] = tv
+    def setoid(self, oid, tv):
+        self.addoid(self, oid, tv)
+    def __iter__(self):
+        return self._oids.__iter__()
+    @property
+    def packet(self):
+        if self._packet == None:
+            return self._pack()
+        else:
+            return self._packet
+    def _pack(self):
+        return pack_tlv(ASN1_SEQ, [
+                                pack_tlv(ASN1_INT, self.ver),
+                                pack_tlv(ASN1_OCTSTR, self.community),
+                                self._pack_payload()
+        ])
+    def _pack_payload(self):
+        return pack_tlv(self.type, [
+                                pack_tlv(ASN1_INT, self.id),
+                                pack_tlv(ASN1_INT, self.err_status),
+                                pack_tlv(ASN1_INT, self.err_id),
+                                self._pack_oids()
+        ])
+    def _pack_oids(self):
+        oids = []
+        for oid in self:
+            t,v = self.getoid(oid)
+            oids.append( pack_tlv(ASN1_SEQ, [
+                                    pack_tlv(ASN1_OID, oid),
+                                    pack_tlv(t,v)
+                                 ]) \
+                       )
+        return pack_tlv(ASN1_SEQ, oids)
     @property
     def ver(self):
         return self._ver
     @ver.setter
     def ver(self, ver):
-        if ver != SNMP_VER1:
-            raise Exception("unsupported value")
-        else:
-            return self._ver
-
-class _SnmpPacket():
-    def __init__(self, version, community, type, request_id):
-        self.version(version)
-        self.community(community)
-        self.type(type)
-    @property
-    def version(self):
-        return self._version
-    @version.setter
-    def version(self, version)
-        self._version = version
+        self._ver = ver
     @property
     def community(self):
         return self._community
     @community.setter
-    def community(self, community)
+    def community(self, community):
         self._community = community
     @property
     def type(self):
@@ -95,21 +139,24 @@ class _SnmpPacket():
     @type.setter
     def type(self, type):
         self._type = type
-
-class _MibCollection():
-    def __init__(self):
-        self._mibs = {}
-    def __setitem__(self, mib, tv):
-        if type(tv) not in (tuple, list):
-            raise Exception("__setitem__ requires tuple")
-        self._mibs[mib] = tv
-    def __getitem__(self, mib):
-        if callable(self._mibs[mib][1]):
-            return self._mibs[mib][0], self._mibs[mib][1]()
-        else:
-            return self._mibs[mib]
-    def pack(self):
-        pass
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, id):
+        self._id = id
+    @property
+    def err_status(self):
+        return self._err_status
+    @err_status.setter
+    def err_status(self, err_status):
+        self._err_status = err_status
+    @property
+    def err_id(self):
+        return self._err_id
+    @err_id.setter
+    def err_id(self, err_id):
+        self._err_id = err_id
 
 def pack(p):
     t,v = p
@@ -128,7 +175,7 @@ def pack_tlv(t, v):
         #octet strings that unpack as strings
         b = bytearray(map(ord, v))
     elif t == ASN1_OCTSTR_BIN:
-        #octet strings that string of hex pairs
+        #octet strings that unpack as string of hex value pairs
         ptr = 0
         b = bytearray()
         while ptr<len(v):
@@ -138,7 +185,7 @@ def pack_tlv(t, v):
     elif t in (ASN1_INT, SNMP_COUNTER, SNMP_GUAGE, SNMP_TIMETICKS):
         #can't eecode -ve (do -ve values occur in snmp?)
         if v < 0:
-            raise Exception("SNMP, -ve int")
+            raise Exception("-ve int")
         else:
             b.append(v & 0xff)
             v //= 0x100
@@ -164,16 +211,15 @@ def pack_tlv(t, v):
                 b.append(id//0x80+0x80)
                 b.append(id&0x7f)
             else:
-                raise ValueError("SNMP, oid out of bounds")
+                raise ValueError("oid chunk out of bounds")
     elif t == SNMP_IPADDR:
         for byte in map(int, v.split(".")):
             b.append(byte)
     elif t in (SNMP_OPAQUE, SNMP_NSAPADDR):
-        raise Exception("SNMP, OPAQUE and NSAPADDR encoding not implemented")
+        raise Exception("SNMP_[OPAQUE & NSAPADDR] not implemented")
     return bytearray([t]) + pack_len(len(b)) + b
 
 def pack_len(l):
-    #msdn.microsoft.com/en-us/library/windows/desktop/bb648641(v=vs.85).aspx
     if l < 0x80:
         return bytearray([l])
     else:
@@ -211,12 +257,14 @@ def unpack_tlv(b):
         #if len == 6 or any 128>byte>31
         #decode as string of hex vals (common case: mac address)
         #else decode as string
+        #Note: printable, but len==6 results in public encoded as hex
         printable = True
         for byte in b[ptr:ptr+1]:
             if not 128>byte>31:
                 printable = False
                 break
-        if l == 6 or not printable:
+        #if l == 6 or not printable:
+        if not printable:
             t = ASN1_OCTSTR_BIN
             v = ""
             for byte in b[ptr : ptr+l]:
@@ -235,7 +283,7 @@ def unpack_tlv(b):
         if b[1]==0 and len(b)==2:
             v=None
         else:
-            raise Exception("SNMP, unpack - bad null encoding")
+            raise Exception("bad null encoding")
     elif t == ASN1_OID:
         #first 2 indexes are encoded in single byte
         v = str( b[ptr]//0x28 ) + "." + str( b[ptr]%0x28 )
@@ -252,13 +300,12 @@ def unpack_tlv(b):
         for byte in b[ptr+1:]:
             v += "." + str(byte)
     elif t in (SNMP_OPAQUE, SNMP_NSAPADDR):
-        raise Exception("SNMP, unpack - OPAQUE & NSAPADDR not implemented")
+        raise Exception("SNMP_[OPAQUE & NSAPADDR] not implemented")
     else:
-        raise Exception("SNMP, unpack - invalid block code encountered")
+        raise Exception("invalid block code", t)
     return t, 1+l+l_incr, v
 
 def unpack_len(v):
-    #msdn.microsoft.com/en-us/library/windows/desktop/bb648641(v=vs.85).aspx
     if v[1]&0x80 == 0x80:
         l = 0
         for i in v[2 : 2+v[1]&0x7f]:
@@ -271,6 +318,6 @@ def hex2str(v):
     ptr = 0
     s = ""
     while ptr<len(v):
-        s += chr( s[ptr:ptr+2] )
+        s += chr( int(v[ptr:ptr+2], 16) )
         ptr += 2
     return s
