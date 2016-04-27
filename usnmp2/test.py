c6a6cda52b9e7ece756c39df5128616160884606
Tim Pelling <tim.pelling@gmail.com>
2016-04-27 20:51:58 +0400

    first pass array mod

---
+++ usnmp2/test.py
@@ -0,0 +1,23 @@
+import binascii
+s="303b02010004067075626c6963a42e06092b0601040181f4690040047f000001020100020100430400000000300f300d06082b06010201020100020121"
+b=bytes(binascii.unhexlify(s))
+
+import usnmp
+usnmp.unpack(b)
+
+import usnmp2
+a=usnmp2.SnmpPacket(b)
+assert len(a) == len(b), "incorrectly reporting packet length"
+a._get_payloadat(3) #should raise exception
+a._get_payloadat(2)
+a._get_payloadat(5)
+a._get_payloadat(13)
+a._get_payloadat(15)
+a._get_payloadat(26)
+usnmp.unpack(bytes(a))
+bytes(a) == b
+usnmp.unpack(b) == usnmp.unpack(b)
+a._buf_extend(62)
+a._buf_extend(65)
+
+
--- usnmp2/usnmp.py
+++ usnmp2/usnmp.py
@@ -56,101 +56,144 @@ SNMP_TRAPGENERIC_AUTHENTICATIONFAILURE = const(0x4)
 SNMP_TRAPGENERIC_EGPNEIGHBORLOSS = const(0x5)
 
 
-#class blob(bytearray):
-#    def insertat(self,ptr,b):
-#        if ptr<0 or ptr>=len(self):
-#            raise IndexError("index out of range")
-#        self.extend( bytearray( len(b)) )
-#        mb = memoryview(self)
-#        for i in range(len(b)):
-#             mb[ptr+len(b)+i] = mb[ptr+i]
-#             mb[ptr+i] = b[i]
-#    def cutat(self,ptr,l):
-#        if ptr<0 or ptr>=len(self):
-#            raise IndexError("index out of range")
-#        mb = memoryview(self)
-#        for i in range(l):
-#            mb[ptr+i] = mb[ptr+l+i]
-            
+class SnmpPacket:
 
-def get_len(b, ptr):
-    if b[ptr+1]&0x80 == 0x80:
-        l = 0
-        offset = 2+b[ptr+1]&0x7f
-        mb = memoryview(b)
-        for i in mb[2 : offset]:b
-            l = l*0x100 + i
+    def __init__(self, b=None, buf=512, blocksize=64):
+        self._blocksize = blocksize
+        if type(b) in (bytearray, bytes):
+            buflen = ((len(b)-1)//self._blocksize+1)*self._blocksize
+            self._b = bytearray(buflen)
+            self._mb = memoryview(self._b)
+            self._mb[0:len(b)] = b
+        else:
+            buflen = ((buf-1)//self._blocksize+1)*self._blocksize
+            self._b = bytearray(buflen)
+            self._mb = memoryview(self._b)
+
+    def _get_lenat(self, ptr):
+        mb = self._mb
+        if mb[ptr+1]&0x80 == 0x80:
+            l = 0
+            offset = 2+mb[ptr+1]&0x7f
+            for i in mb[2 : offset]:
+                l = l*0x100 + i
+        else:
+            l = mb[ptr+1]
+            offset = 2
         del(mb)
-    else:
-        l = b[ptr+1]
-        offset = 2
-    return l, offset
+        return l, offset
 
-def get_payload(b, ptr):
-    t = b[ptr]
-    l, offset = get_len(b,ptr)
-    end = ptr + l
-    ptr += offset
-    mb = memoryview(b)
-    #sequence types
-    if t in (ASN1_SEQ, \
-             SNMP_GETREQUEST, SNMP_GETRESPONSE, SNMP_GETNEXTREQUEST, \
-             SNMP_SETREQUEST, SNMP_TRAP):
-        v = []
-        while ptr < end:
-            lb, lb_offset = get_len( mb, ptr )
-            v.append( bytes(mb[ptr : ptr+lb_offset+lb]) )
-            ptr += lb + lb_offset
-    #octet string
-    elif t == ASN1_OCTSTR:
-        #if binary data contains unprintables (e.g. a mac-addr)
-        #decode as a string of hex value pairs
-        #else decode as python string
-        printable = True
-        for byte in mb[ptr:]:
-            if not 128>byte>31:
-                printable = False
-                break
-        if not printable:
-            v = ""
-            for byte in mb[ptr : ptr+l]:
-                if byte<0x10:
-                    v += '0' + hex(byte)[2:]
-                else:
-                    v += hex(byte)[2:]
-        else:
-            v = bytes(mb[ptr : ptr+l]).decode()
-    #integers
-    elif t in (ASN1_INT, SNMP_COUNTER, SNMP_GUAGE, SNMP_TIMETICKS):
-        v = 0
-        for byte in mb[ptr : ptr+l]:
-            v = v*0x100 + byte
-    #null
-    elif t == ASN1_NULL:
-        if mb[ptr-1]==0 and l==0:
-            v = None
-        else:
-            raise Exception("bad null encoding")
-    #oid
-    elif t == ASN1_OID:
-        #first 2 indexes are encoded in single byte
-        v = str( mb[ptr]//0x28 ) + "." + str( mb[ptr]%0x28 )
-        ptr += 1
-        high_septet = 0
-        for byte in mb[ptr:]:
-            if byte&0x80 == 0x80:
-                high_septet = byte - 0x80
+    def _get_payloadat(self, ptr):
+        mb = self._mb
+        t = mb[ptr]
+        l, offset = self._get_lenat(ptr)
+        end = ptr + offset + l
+        ptr += offset
+        #sequence types
+        if t in (ASN1_SEQ, \
+                 SNMP_GETREQUEST, SNMP_GETRESPONSE, SNMP_GETNEXTREQUEST, \
+                 SNMP_SETREQUEST, SNMP_TRAP):
+            v = []
+            while ptr < end:
+                lb, lb_offset = self._get_lenat(ptr)
+                v.append( bytes( mb[ptr : ptr+lb_offset+lb]) )
+                ptr += lb + lb_offset
+        #octet string
+        elif t == ASN1_OCTSTR:
+            #if binary data contains unprintables (e.g. a mac-addr)
+            #decode as a string of hex value pairs
+            #else decode as python string
+            printable = True
+            for byte in mb[ptr:end]:
+                if not 128>byte>31:
+                    printable = False
+                    break
+            if not printable:
+                v = ""
+                for byte in mb[ptr : ptr+l]:
+                    if byte < 0x10:
+                        v += '0' + hex(byte)[2:]
+                    else:
+                        v += hex(byte)[2:]
             else:
-                v += "." + str(high_septet*0x80 + byte)
-                high_septet = 0
-    #ip address            
-    elif t == SNMP_IPADDR:
-        v = str( mb[ptr] )
-        for byte in mb[ptr+1 : ptr+len]:
-            v += "." + str(byte)
-    elif t in (SNMP_OPAQUE, SNMP_NSAPADDR):
-        raise Exception("SNMP_[OPAQUE & NSAPADDR] not implemented")
-    else:
-        raise Exception("invalid type", t)
-    del(mb)
-    return v
+                v = bytes(mb[ptr : ptr+l]).decode()
+        #integers
+        elif t in (ASN1_INT, SNMP_COUNTER, SNMP_GUAGE, SNMP_TIMETICKS):
+            v = 0
+            #for byte in mb[ptr : ptr+l]:
+            for byte in mb[ptr:end]:
+                v = v*0x100 + byte
+        #null
+        elif t == ASN1_NULL:
+            if mb[ptr-1]==0 and l==0:
+                v = None
+            else:
+                raise BadNullEncoding
+        #oid
+        elif t == ASN1_OID:
+            #first 2 indexes are encoded in single byte
+            v = str( self._mb[ptr]//0x28 ) + "." + str( self._mb[ptr]%0x28 )
+            ptr += 1
+            high_septet = 0
+            for byte in mb[ptr:end]:
+                if byte&0x80 == 0x80:
+                        high_septet = byte - 0x80
+                else:
+                    v += "." + str(high_septet*0x80 + byte)
+                    high_septet = 0
+        #ip address
+        elif t == SNMP_IPADDR:
+            v = ""
+            for byte in mb[ptr:end]:
+                if len(v) > 0:
+                    v += "." + str(byte)
+                else:
+                    v += str(byte)
+        elif t in (SNMP_OPAQUE, SNMP_NSAPADDR):
+            raise UnimplementedType(t)
+        else:
+            raise InvalidType(t)
+        del(mb)
+        return t, v
+
+    def __len__(self):
+        return sum(self._get_lenat(0))
+    
+    def __bytes__(self):
+        return bytes( self._mb[:len(self)] )
+
+    #extend buffer (if necessary) to nearest multiple
+    #of _blocksize that will accomodate size
+    def _buf_extend(self, size):
+        idealsize = ((size-1)//self._blocksize+1)*self._blocksize
+        if idealsize > len(self._b):
+            del(self._mb)
+            self._b.extend( bytearray(size-len(self._b)) )
+            self._mb = memoryview(self._b)
+
+    #serves _insertat if passed l=0
+    #serves _cutat if passed b where len(b)==0 aka b''
+    def _replaceat(self, ptr, l, b):
+        pl, poffset = self._get_lenat(0)
+        self._buf_extend( pl+offset+l )
+        mb = self._mb
+        vector = l-len(b)
+        for i in range(abs(vector)):
+            if vector>0:
+                mb[pl+l-i] = mb[pl-i]
+            else:
+                mb[pl-i] = mb[pl+l-i]
+        del(mb)
+
+
+class InvalidType(Exception):
+    pass
+
+class UnimplementedType(Exception):
+    pass
+
+class BadNullEncoding(Exception):
+    pass
+
+class InternalError(Exception):
+    pass
