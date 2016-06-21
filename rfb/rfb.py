459ad7398dac1b9eba553706dd5d8fed7bbf13f3
Tim Pelling <tim.pelling@gmail.com>
2016-06-21 20:33:21 +0400

    session init success

--- rfb/rfb.py
+++ rfb/rfb.py
@@ -1,12 +1,8 @@
 #https://github.com/rfbproto/rfbproto/blob/master/rfbproto.rst
-import socket
-try:
-    import struct
-except:
-    import ustruct as struct
+import socket, uctypes
+#import struct
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(('127.0.0.1',5902))
 s.bind(('',5900))
 s.listen(3)
 
@@ -22,7 +18,7 @@ d = conn.recv(1024)
 if d == ProtocolVersion:
     print('client accepted proposed version')
 
-
+ 
 #Security
 #--------
 #v3.3 server decides and sends type as single U32
@@ -30,46 +26,107 @@ if d == ProtocolVersion:
 #           U8 # of types
 #           U8 Array of typescodes, of len '# of types'
 #       client decides by sending a U8 for typecode 
-SEC_INVALID = 0x0
+#SEC_INVALID = 0x0
+#SEC_VNCAUTH = 0x2
+#SEC_TIGHT = 0x10
+#SEC_VENCRYPT = 0x13
 SEC_NONE = 0x1
-SEC_VNCAUTH = 0x2
-SEC_TIGHT = 0x10
-SEC_VENCRYPT = 0x13
 
-conn.send( struct.pack('!I', SEC_NONE) )
+#conn.send( struct.pack('!I', SEC_NONE) )
+conn.send( int(SEC_NONE).to_bytes(4, 'big') )
 
 #ClientInit
 #----------
 d = conn.recv(1024)
 if d[0] == 1:
-    print('other clients can be left connected')
+    print('client accepted: leave other clients connected')
 elif d[0] == 0:
-    print('disconnect other clients')
+    print('client accepted: disconnect other clients')
 else:
-    print('invalid ClientInit from client')
+    print('invalid ClientInit')
 
 #ServerInit
 #----------
-desk_name = b'desktop'
+#svr_init = struct.pack('!2H4B3H3B', 20, 20, 32, 24, 1, 1, 0xff, 0xff, 0xff, 0, 8, 16) + bytes(3) + bytes(4)
+#conn.send(svr_init)
 
-#framebuffer width
-#framebuffer height
-#pixel format:
-#   bits per pixel 8,16 or 32
-#   depth = used bits per pixel for e.g. 24 for r,g,b 8
-#   big-endian flag >0=True
-#   true-color flag >0=True - 0 == colormap required
-#   red, green and blue max values
-#   red, green and blue bit shift values
-#   3 bytes of padding
-#length of desktop name
-#desktop name
+name = b'desktop' #cannot be changed, as length used to define structure template (wobbly)
 
-#svr_init = struct.pack('! 2H 4B 3H 3B 3x I ' + str(len(desk_name)) + 's', 20, 20, 32, 24, 1, 1, 0xff, 0xff, 0xff, 0, 8, 16, len(desk_name), desk_name)
+si_desc = {
+    'w': uctypes.UINT16 | 0,            #framebuffer width
+    'h': uctypes.UINT16 | 2,            #framebuffer height
+        #PixelFormat
+        'bpp': uctypes.UINT8 | 4,           #bits per pixel (sent) valid = 8,16,32
+        'depth': uctypes.UINT8 | 5,         #bits per pixel (actually used)
+        'big_endian': uctypes.UINT8 | 6,    #big_endian if >1
+        'true_colour':  uctypes.UINT8 | 7,  #true_color if >1, else must supply colourmap
+        'r_max': uctypes.UINT16 | 8,        #bitmask red value
+        'g_max': uctypes.UINT16 | 10,       #bitmask green value
+        'b_max': uctypes.UINT16 | 12,       #bitmast blue value
+        'r_shift': uctypes.UINT8 | 14,      #bit shift for red value
+        'g_shift': uctypes.UINT8 | 15,      #bit shift for green value
+        'b_shift': uctypes.UINT8 | 16,      #bit shift for blue value
+        '': (uctypes.ARRAY | 17, uctypes.UINT8 | 3), #3 pad bytes
+    #length of desktop name, and name
+    #due to encoding into struct, cannot be changed 
+    'len_name': uctypes.UINT32 | 20,
+    'name': (uctypes.ARRAY | 24, uctypes.UINT8 | len(name))  
+}
 
-svr_init = struct.pack('!2H4B3H3B', 20, 20, 32, 24, 1, 1, 0xff, 0xff, 0xff, 0, 8, 16)
+si_data = bytearray( uctypes.sizeof(si_desc) )
 
-conn.send(svr_init)
+si = uctypes.struct(
+    uctypes.addressof(si_data),
+    si_desc,
+    uctypes.BIG_ENDIAN
+)
+
+si.w = 24
+si.h = 26
+si.bpp = 32
+si.depth = 24
+si.big_endian = 1
+si.true_colour = 1
+si.r_max = 255
+si.g_max = 255
+si.b_max = 255
+si.r_shift = 0
+si.g_shift = 8
+si.b_shift = 16
+si.len_name = len(name)
+
+for i,j in enumerate(name):
+    si.name[i] = j
+
+#conn.send( bytes(svr_init) )
+conn.send( si_data )
+
+#SetColourMapEntries
+#-------------------
+#follows ServerInit _if_ true_colour = 0
+colours = ((0,0,0), (255,0,0), (0,255,0), (0,0,255), (255,255,255))
+
+scme_hdr = int(1).to_bytes(1, 'big') \
+           + int(0).to_bytes(1, 'big') \
+           + int(1).to_bytes(2, 'big') \
+           + int( len(colours) ).to_bytes(2, 'big') \
+
+#conn.send(scme_hrd)
+
+scme_clr_desc = {
+    'r': uctypes.UINT16 | 0,
+    'g': uctypes.UINT16 | 2,
+    'b': uctypes.UINT16 | 4,
+}
+
+scme_clr_data = bytearray( uctypes.sizeof(scme_clr_desc) )
+scme_clr = uctypes.struct(uctypes.addressof(scme_clr_data), scme_clr_desc, uctypes.BIG_ENDIAN)
+
+for (r,g,b) in colours:
+    scme_clr.r = r
+    scme_clr.g = g
+    scme_clr.b = b
+    print(scme_clr_data)
 
 #FrameBufferUpdate
 SVR_MSG_TYPE_FRAMEBUFFERUPDATE = 0x0
@@ -77,15 +134,15 @@ rect_count = 1
 
 #msg is padded with a zero byte
 msg = bytes([SVR_MSG_TYPE_FRAMEBUFFERUPDATE,0]) \
-      + rect_count.to_bytes(2, 'big')
+    + rect_count.to_bytes(2, 'big')
 
 x,y = 0,0
 encoding = 0 #raw
 rect = (x).to_bytes(2, 'big') \
-       + (y).to_bytes(2, 'big') \
-       + (w).to_bytes(2, 'big') \
-       + (h).to_bytes(2, 'big') \
-       + (encoding).to_bytes(4, 'big') \
+    + (y).to_bytes(2, 'big') \
+    + (w).to_bytes(2, 'big') \
+    + (h).to_bytes(2, 'big') \
+    + (encoding).to_bytes(4, 'big') \
 
 pixels = bytes(w*h*4) #all zero as of now = black
 
--- rfb/rfb_comp.py
+++ rfb/rfb_comp.py
@@ -2,10 +2,12 @@ import socket, struct
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('',5900))
-s.listen(3)
+s.listen(1)
 
 conn, (caddr, cport) = s.accept()
 print('connection from ' + caddr + '::' + str(cport))
+
+s.settimeout(0)
 
 ProtocolVersion = b'RFB 003.003\n'
 conn.send(ProtocolVersion)
@@ -20,17 +22,50 @@ d = conn.recv(1024)
 print( 'received ClientInit(' + str(d[0]) + ')' )
 
 w,h = 800,600
-svr_init = struct.pack('!2H4B3H3B', w, h, 32, 24, 1, 1, 0xff, 0xff, 0xff, 0, 8, 16) + bytes(3) + bytes(4)
+name=b'desktop'
+svr_init = struct.pack('!2H4B3H3B', w, h, 32, 24, 1, 1, 0xff, 0xff, 0xff, 0, 8, 16) + bytes(3) + bytes([0,0,0,len(name)]) + name
 conn.send(svr_init)
 print('sent ServerInit:', svr_init)
 
-#encoding = 0
-#rect_count = 1
-#x,y = 0,0
-#msg = bytes([0x0,0]) + rect_count.to_bytes(2, 'big')
-#rect = (x).to_bytes(2, 'big') \
-#       + (y).to_bytes(2, 'big') \
-#       + (w).to_bytes(2, 'big') \
-#       + (h).to_bytes(2, 'big') \
-#       + (encoding).to_bytes(4, 'big')
-#conn.send(msg + rect + bytes(w*h*4))
+def square(x,y,w,h,rgb):
+    b = bytes([0,0,0,1]) #update, 1 rect
+    b += x.to_bytes(2, 'big') \
+         + y.to_bytes(2, 'big') \
+         + w.to_bytes(2, 'big') \
+         + h.to_bytes(2, 'big') \
+         + int(0).to_bytes(4, 'big')
+    b += (bytes(rgb)+b'\x00')*w*h
+    conn.send(b)
+
+square(0,0,800,600,(0,0,0))
+
+i=0
+while True:
+    d = conn.recv(1024)
+    if not d is None:
+        if d[0] == 0x5:
+            i += 1 if i<255 else -254
+            print(d[1])
+            x = int.from_bytes(d[2:4],'big')
+            y = int.from_bytes(d[4:6],'big')
+            if x>5 and x<795 and y>5 and y<595:
+                square(x-5, y-5,10,10,(i,i,i))
+
+
+
+
+
+
+encoding = 0
+rect_count = 1
+msg = bytes([0x0,0]) + rect_count.to_bytes(2, 'big')
+w,h = 10,10
+for i in range(255):
+    x = y = i
+    clr = bytes([i,i,i,0])
+    rect = (x).to_bytes(2, 'big') \
+           + (y).to_bytes(2, 'big') \
+           + (w).to_bytes(2, 'big') \
+           + (h).to_bytes(2, 'big') \
+           + (encoding).to_bytes(4, 'big')
+    conn.send(msg + rect + (clr*w*h))
