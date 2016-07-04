91333940141348602ac080890e1a09f700ac2d5a
Tim Pelling <tim.pelling@gmail.com>
2016-07-04 08:36:18 +0400

    doc updates

--- rfb/example.py
+++ rfb/example.py
@@ -1,5 +1,5 @@
 import rfb
-# select the correct random lib for micropython/cpython
+# import the getrandbits for micropython or cpython
 try:
     from urandom import getrandbits
 except:
@@ -11,7 +11,7 @@ class my_session(rfb.RfbSession):
         x,y = getrandbits(8), getrandbits(8)
         w = h = getrandbits(5)
         # co-erce x,y to ensure rectangle doesn't overflow
-        # framebuffer size (client will error out if they do)
+        # framebuffer size (client will error out if it does)
         x = x if x<self.w-w else x-w
         y = y if y<self.h-h else y-h
         bgcolour = (getrandbits(8), getrandbits(8), getrandbits(8))
@@ -20,11 +20,13 @@ class my_session(rfb.RfbSession):
                 x, y, 
                 w, h, 
                 bgcolour,
-                # refer documentation hereunder!
+                # let the rectangle know the session properties
+                # required for encoding to bytes
                 self.bpp, self.depth, self.true,
             )
         ]
         # send a framebuffer update to the client
+        # ServerFrameBufferUpdate requires a list of rectangles to send ...
         self.send( rfb.ServerFrameBufferUpdate( rectangles ) )
 
 svr = rfb.RfbServer(255, 255, handler=my_session, name=b'custom')
