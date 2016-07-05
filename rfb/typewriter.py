6904fee1711219e0bb62871bcf174785f448957e
Tim Pelling <tim.pelling@gmail.com>
2016-07-05 20:39:33 +0400

    optimisations

--- rfb/typewriter.py
+++ rfb/typewriter.py
@@ -10,17 +10,39 @@ class TypeWriter(rfb.RfbSession):
         super().__init__(conn, w, h, name)
         self.rectangles = []
         self.char = rfb.RawRect(
-                        -font.w, # -ve font width, first char will increment   
-                        self.h - font.h,
+                         # -ve font width, first char will increment
+                        -font.w, self.h-font.h,
                         font.w, font.h,
                         self.bpp, self.depth, 
                         self.big, self.true,
                         self.masks, self.shifts
                     )
+        # pre-initialise rectangles required by main loop saves mem
+        self.scroll = rfb.CopyRect(
+                        0, 0, 
+                        self.w, self.h-font.h,
+                        0, font.h
+                      )
+        self.clear = rfb.RRERect(
+                        0, self.h-font.h,
+                        self.w, font.h,
+                        (0,0,0),
+                        self.bpp, self.depth, 
+                        self.big, self.true,
+                        self.masks, self.shifts
+                     )
+        self.backspace = rfb.RawRect(
+                            0, self.h-font.h,
+                            font.w, font.h,
+                            self.bpp, self.depth, 
+                            self.big, self.true,
+                            self.masks, self.shifts
+                         )
+
 
     def update(self):
         self.send( rfb.ServerFrameBufferUpdate( self.rectangles ) )
-        # chuck rectangles sent to display
+        # chuck rectangles already sent
         self.rectangles.clear()
 
     def ClientKeyEvent(self, down, key):
@@ -30,11 +52,10 @@ class TypeWriter(rfb.RfbSession):
             try:
                 bits = font.getbitmap_str(key)
                 for idx, bit in enumerate(bits):
-                    colour = bytes((255,255,255) if int(bit) else (0,0,0))
-                    start = idx*(self.bpp//8)
-                    # TODO: fix this to use setpixel ...
-                    # refer current documentation method
-                    self.char.buffer[start : start+3] = colour 
+                    self.char.setpixel(
+                        idx%font.w, idx//font.w,
+                        (255,255,255) if int(bit) else (0,0,0)
+                    )
                 self.rectangles.append( self.char )
                 self.char.x += font.w
             except:
@@ -46,50 +67,18 @@ class TypeWriter(rfb.RfbSession):
             
             if key == 65288: # <backspace>
                 if self.char.x >= 0:
-                    self.rectangles.append( 
-                        rfb.RawRect(
-                                self.char.x, self.h-font.h,
-                                font.w, font.h,
-                                self.bpp, self.depth, 
-                                self.big, self.true,
-                                self.masks, self.shifts
-                        )
-                    )
-                    self.rectangles[-1].fill((0,0,0))
+                    self.backspace.x = self.char.x 
+                    self.rectangles.append( self.backspace )
                 self.char.x -= font.w
         
+            # wrap line at width
             if self.char.x >= self.w-font.w:
                 self.CarriageReturn()
                 self.char.x = -font.w
 
     def CarriageReturn(self):
-        # scroll
-        self.rectangles.append(
-            rfb.CopyRect(
-                0, 0, 
-                self.w, self.h-font.h,
-                0, font.h
-            )
-        )
-        # clear, use CopyRect to minimise mem and traffic
-        self.rectangles.append(
-            rfb.RawRect(
-                0, self.h-font.h,
-                font.w, font.h,
-                self.bpp, self.depth, 
-                self.big, self.true,
-                self.masks, self.shifts
-            ) 
-        )
-        self.rectangles[-1].fill((0,0,0))
-        for x in range(1, self.w//font.w):
-            self.rectangles.append(
-                rfb.CopyRect(
-                    x*font.w, self.h-font.h,
-                    font.w, font.h,
-                    0, self.h-font.h
-                )
-            )
+        self.rectangles.append( self.scroll )
+        self.rectangles.append( self.clear )
 
 
 svr = rfb.RfbServer(
