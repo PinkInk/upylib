4a1040bcbf70662d885e6ff5c5b87ee289c5057c
Tim Pelling <tim.pelling@gmail.com>
2016-07-25 05:55:27 +0400

    experimental httpd WIP

---
+++ http/http/__init__.py
@@ -0,0 +1,44 @@
+from http.connection import *
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+try: # mpy/cpython compat in main loop
+    BlockingIOError
+except:
+    class BlockingIOError(Exception):
+        pass
+
+
+class HttpServer():
+
+    def __init__(self,
+                 handler = HttpConnection,
+                 addr = ('0.0.0.0', 80),
+                 backlog = 3
+                ):
+        self.handler = HttpConnection
+        self.connections = []
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setblocking(False)
+        self.s.bind( socket.getaddrinfo(addr[0], addr[1])[0][-1] )
+        self.s.listen(backlog)
+    
+    def serve(self):
+        while True:
+            # microcontroller should probably sleep here
+            self.accept()
+            self.service()
+    
+    def accept(self):
+        try:
+            self.connections.append( self.handler( self.s.accept() ) )
+        except (OSError, BlockingIOError):
+            pass
+    
+    def service(self):
+        for idx,connection in enumerate( self.connections ):
+            if not connection.service_requests():
+                del( self.connections[idx] )
