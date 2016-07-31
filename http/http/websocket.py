08013174f0417ed13318b42dfd22db809b74fd16
Tim Pelling <tim.pelling@gmail.com>
2016-07-31 19:48:32 +0400

    http/websocket.py cleanup

--- http/http/websocket.py
+++ http/http/websocket.py
@@ -1,6 +1,5 @@
 # https://tools.ietf.org/html/rfc6455
 
-import httpd.parse as parse
 from sha1 import sha1
 
 try:
@@ -13,6 +12,13 @@ try:
 except:
     from collections import namedtuple 
 
+def bytes_to_int(b): #big-endian
+    i = 0
+    for b8 in b:
+        i <<= 8
+        i += b8
+    return i
+
 Frame = namedtuple("Frame", ("final", "opcode", "msg"))
 
 OP_CONT = 0x0 # continuation frame
@@ -21,16 +27,6 @@ OP_BIN = 0x2 # binary frame
 OP_CLOSE = 0x8 # close connection
 OP_PING = 0x9 # ping frame
 OP_PONG = 0xa # pong frame
-
-# NOTE: not reqd whilst we are not using, 
-#       therefore not decoding, frame length
-# micropython int.from_bytes doesn't support big-endian
-# def bytes_to_int(b): #big-endian
-#     i = 0
-#     for b8 in b:
-#         i <<= 8
-#         i += b8
-#     return i
 
 
 class WebSocketError(Exception):
@@ -41,35 +37,33 @@ class WebSocket:
 
     MAGIC = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
-    def __init__(self, conn, request, buflen=128):
-        self.conn,self.addr = conn
-        # TODO: is memoryview waranted?
+    def __init__(self, request, conn, buflen=128):
+        self.conn = conn
         self._buf = bytearray(buflen)
         self.buf = memoryview(self._buf)
 
-        # negotiate websocket
-        # ASSUMES: caller tested is_websocket_request
+        # negotiate websocket, assumes caller tested is_websocket_request
         options = { "Upgrade": "websocket", "Connection": "Upgrade" }
         options["Sec-WebSocket-Accept"] = \
                 b2a_base64(unhexlify(
                     sha1(request.options["Sec-WebSocket-Key"] + self.MAGIC)
                 ))[:-1] # skip trailing newline
-        # client will fail negotation if extensions 
-        # or protocol don't match the  request
+        # client will fail if extensions or protocol don't match request
+        self.extensions = self.protocol = ""
         if "Sec-Websocket-Extensions" in request.options:
-            options["Sec-WebSocket-Extensions"] = \
+            self.extensions = options["Sec-WebSocket-Extensions"] = \
                 request.options["Sec-WebSocket-Extensions"]
         if "Sec-WebSocket-Protocol" in request.options:
-            options["Sec-WebSocket-Protocol"] = \
+            self.protocol = options["Sec-WebSocket-Protocol"] = \
                 bytes(request.options["Sec-WebSocket-Protocol"]
         
-        self.send("HTTP/1.x 101 Switching Protocols\r\n")
+        self.conn.send("HTTP/1.1 101 Switching Protocols\r\n")
         for key in options:
-            self.send(
+            self.conn.send(
                 bytes(key, "utf-8") + b":" \
                 + bytes(options[key], "utf-8") + b"\r\n"
             )
-        self.send(b"\r\n")
+        self.conn.send(b"\r\n")
     
     def service_frames(self):
         frame = self.recvframe()
@@ -80,11 +74,10 @@ class WebSocket:
                 pass # ignore
 
     def sendframe(self, opcode, msg, final=True):
-        # TODO: handling of ping, pending pong?
         b = bytes([(fin<<7) + opcode ])
         length = len(msg)
         if length < 126:
-            b += bytes([length]) # bytes([ (use_mask<<7) + length ])
+            b += bytes([length])
         # TODO : unuglify -->
         elif length <= 0xffff:
             b += bytes([ 0, length>>8, length&0xff ])
@@ -97,8 +90,7 @@ class WebSocket:
                       ])
         else:
             raise WebSocketError("Message too long")
-        # server to client msgs don't use mask
-        # concat converts str msg to bytes
+        # server to client doesn't use mask, concat coerces str msg to bytes
         self.send( b + msg )
 
     def recvframe(self):
@@ -110,14 +102,13 @@ class WebSocket:
             opcode = mf[ptr] & 0b1111
             ptr += 1
             use_mask = bool( mf[ptr] & (1<<7) )
-            # TODO: use length in decode-loop
-            #       meanwhile don't decode, skip
+
             length = mf[ptr] & 0x7f
             if length == 126:
-                # length = bytes_to_int( mf[ptr+1:ptr+1+2] )
+                length = bytes_to_int( mf[ptr+1:ptr+1+2] )
                 ptr += 1+2
             elif length == 127:
-                # length = bytes_to_int( mf[ptr+1:ptr+1+4] )
+                length = bytes_to_int( mf[ptr+1:ptr+1+4] )
                 ptr += 1+4
             else:
                 ptr += 1
@@ -125,10 +116,9 @@ class WebSocket:
             ptr += 4
             if use_mask:
                 msg = b""
-                # decode-loop, ignores encoded frame length
-                # and assumes balance bytes are encoded msg
-                for i in range(len(mf[ptr:])):
-                    msg += bytes([ mf[ptr+i]^mask[i%4] ]) # chr( mf[ptr+i]^mask[i%4] )
+                # for i in range(len(mf[ptr:])):
+                for i in range(length):
+                    msg += bytes([ mf[ptr+i]^mask[i%4] ])
                 return Frame(final, opcode, msg) 
             else:
                 raise WebSocketError("Unmasked client websocket frame")
@@ -143,7 +133,3 @@ class WebSocket:
             else:
                 break
         return b
-
-    def send(self, b):
-        if b:
-            self.conn.send(b)
